f4d18382c330268f05c7ba2080b60905856d2bc9
Miroslav Zeleny <m.zeleny@volny.cz>
2024-05-15 17:53:20 +0200

    Homework1

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class Main {
     public static void ukol1() {
         System.out.println("Hello world!");
@@ -29,25 +32,27 @@ public class Main {
         // Má vypsat "1.0". Ale vypíše "0.9999" !!!
     }
 
-    /*public static void ukol6() {
-        BigDecimal cena = 0;
-        cena = cena.add(BigDecimal.valueOf(0.1));
-        System.out.println(cena);*/
+    public static void ukol6() {
+        BigDecimal cena = BigDecimal.ZERO;  // Inicializace proměnné BigDecimal
+        for (int i = 0; i < 10; i++) {
+            cena = cena.add(BigDecimal.valueOf(0.1));
+        }
+        System.out.println(cena);
    }
 
-    /*public static void ukol7() {
+    public static void ukol7() {
         int vykonMotoru = 120;
         System.out.println("Výkon Motoru je: "+vykonMotoru+" kW.");
-    }*/
+    }
 
-    /*public static void ukol8() {
+    public static void ukol8() {
         int velikostKosile = 37;
         System.out.println("Velikost košile je: "+ velikostKosile+ ".");
-    }*/
+    }
 
-    /*public static void ukol9() {
+    public static void ukol9() {
         System.out.println("Metoda *skoro* bez chybičky!");
-    }*/
+    }
 
     public static void main(String[] args) {
         System.out.println("Oprav chyby a spusť všechny metody!");
@@ -64,4 +69,15 @@ public class Main {
         ukol7();
         ukol8();
         ukol9();
-    }
+
+        String Name;
+        String Surname;
+        LocalDate birthdate;
+        int numberOfContracts;
+        double weightOfCarrot;
+        String cityOfSeat;
+        String registrationPlate;
+        double consumption;
+        String IPAddress;
+
+    }}
